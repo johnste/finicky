2e6c640eb163074c39cf9197006932e53f6270c8
John Sterling <john@johnste.se>
2019-10-29 09:51:35 +0100

    simpler...

--- Finicky/Finicky/Browsers.swift
+++ Finicky/Finicky/Browsers.swift
@@ -1,6 +1,5 @@
 
 import Foundation
-import Carbon
 
 enum Browser: String {
     case Chrome = "com.google.Chrome"
@@ -17,25 +16,19 @@ public func startBrowser(app: BrowserOpts, url: URL, defaultOpenInBackground: Bo
 
     if (app.bundleId == Browser.Chrome.rawValue) {
         let chromeScript = """
-            on openIncognito(newurl)
-                if application "Google Chrome" is running then
-                    tell application "Google Chrome"
-                        set incognitoWindows to (every window whose mode is "incognito")
-
-                        if (count of incognitoWindows) > 0 then
-                            set incognitoWindow to first window whose mode is "incognito"
-                            tell incognitoWindow to make new tab at after (get active tab) with properties {URL:newurl}
-                        else
-                            set incognitoWindow to make new window with properties {mode:"incognito"}
-                            tell incognitoWindow to set URL of active tab to newurl
-                        end if
-                    end tell
+            tell application "Google Chrome"
+                set incognitoWindows to (every window whose mode is "incognito")
+                set myLink to "http://example.com/?shark=yeswindow"
+                if (count of incognitoWindows) > 0 then
+                    set incognitoWindow to first window whose mode is "incognito"
+                    tell incognitoWindow to make new tab at after (get active tab) with properties {URL:myLink}
                 else
-                    do shell script "open -a \"Google Chrome\" --args {URL: newurl} --incognito"
+                    set incognitoWindow to make new window with properties {mode:"incognito"}
+                    tell incognitoWindow to set URL of active tab to myLink
                 end if
-            end openIncognito
+            end tell
         """
-        executeScript(chromeScript, url: url.absoluteString)
+        executeScript(chromeScript)
         return
     }
 
@@ -48,38 +41,16 @@ public func startBrowser(app: BrowserOpts, url: URL, defaultOpenInBackground: Bo
     shell(command)
 }
 
-func executeScript(_ script: String, url: String) {
-    let parameters = NSAppleEventDescriptor.list()
-    parameters.insert(NSAppleEventDescriptor(string: url), at: 0)
-
-    let scriptObject = NSAppleScript(source: script)
+func executeScript(_ script: String) {
     var error: NSDictionary?
-    scriptObject?.compileAndReturnError(&error)
-
-
-    let event = NSAppleEventDescriptor(
-        eventClass: AEEventClass(kASAppleScriptSuite),
-        eventID: AEEventID(kASSubroutineEvent),
-        targetDescriptor: nil,
-        returnID: AEReturnID(kAutoGenerateReturnID),
-        transactionID: AETransactionID(kAnyTransactionID)
-    )
-    event.setDescriptor(NSAppleEventDescriptor(string: "openIncognito"), forKeyword: AEKeyword(keyASSubroutineName))
-    event.setDescriptor(parameters, forKeyword: AEKeyword(keyDirectObject))
-
-    print("hello")
-    let result = scriptObject?.executeAppleEvent(event, error: &error)
-
-
-//    if let scriptObject = NSAppleScript(source: script) {
-//
-//        if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
-//                                                                           &error) {
-//            print(output.stringValue!)
-//        } else if (error != nil) {
-//            showNotification(title: "error: \(String(describing: error))")
-//        }
-//    }
+    if let scriptObject = NSAppleScript(source: script) {
+        if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
+                                                                           &error) {
+            print(output.stringValue)
+        } else if (error != nil) {
+            showNotification(title: "error: \(error)")
+        }
+    }
 }
 
 
