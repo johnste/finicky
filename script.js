965304401420a71cc8f9afb4c79e073449b146bf
John Sterling <john@johnste.se>
2020-06-04 23:05:18 +0200

    Fix script

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 async function showDownloads() {
-  let response = fetch("https://api.github.com/repos/johnste/finicky/releases");
+  let response = await fetch(
+    "https://api.github.com/repos/johnste/finicky/releases"
+  );
   let result = await response.json();
 
   let total = 0;
 
   const downloads = Object.fromEntries(
     result
-      .filter((v) => current?.assets?.[0])
-      .map((v) => [current.tag_name, current.assets[0].download_count])
+      .filter((v) => v?.assets?.[0])
+      .map((v) => [v.tag_name, v.assets[0].download_count])
   );
 
-  document.getElementsByClassName("download-count").innerHTML = JSON.stringify(
-    { total, ...downloads },
-    null,
-    2
-  );
+  console.log(downloads);
+  document.getElementsByClassName(
+    "download-count"
+  )[0].innerHTML = JSON.stringify({ total, ...downloads }, null, 2);
 }
 
 async function showStarGazers() {
-  let response = fetch("https://api.github.com/repos/johnste/finicky");
+  let response = await fetch("https://api.github.com/repos/johnste/finicky");
   let result = await response.json();
 
-  document.getElementsByClassName(".star-count").innerHTML =
+  console.log(result.stargazers_count);
+
+  document.getElementsByClassName("star-count")[0].innerHTML =
     result.stargazers_count;
 }
 
