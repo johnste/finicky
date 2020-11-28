87497b8fab7d504c1886e7a8bcaee040ea5cec9a
Igor Savelev <igor9774@igor9774@gmail.com>
2020-11-28 15:25:33 +0000

    Added example handler to default config

--- Finicky/Finicky/defaultConfig.js
+++ Finicky/Finicky/defaultConfig.js
@@ -2,5 +2,17 @@
 // Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
 
 module.exports = {
-  defaultBrowser: "Safari"
+  defaultBrowser: "Safari",
+/*
+  handlers: [
+    {
+      // Open google.com and *.google.com urls in Google Chrome
+      match: [
+        "google.com*", // match google.com urls
+        finicky.matchDomains(/.*\.google.com/) // use helper function to match on domain only
+      ],
+      browser: "Google Chrome"
+    }
+  ]
+*/
 }
