29d081f0df3dff7e675bf447fae3a57df60b89cc
John Sterling <john.sterling@soundtrackyourbrand.com>
2025-03-03 22:39:49 +0100

    feat: warn about matchDomains being deprecated #345

--- config-api/src/utilities.ts
+++ config-api/src/utilities.ts
@@ -1,35 +1,38 @@
-type Matcher = string | RegExp
+type Matcher = string | RegExp;
 
 export function matchHostnames(matchers: Matcher | Matcher[]) {
+  matchers = Array.isArray(matchers) ? matchers : [matchers];
 
-    matchers = Array.isArray(matchers) ? matchers : [matchers];
-  
-    matchers.forEach((matcher) => {
-      if (matcher instanceof RegExp || typeof matcher === "string") {
-        return;
+  matchers.forEach((matcher) => {
+    if (matcher instanceof RegExp || typeof matcher === "string") {
+      return;
+    }
+    throw new Error(`Unrecognized hostname type "${typeof matcher}"`);
+  });
+
+  return function (url: URL) {
+    const hostname = url.hostname;
+
+    if (!hostname) {
+      console.warn("No hostname available for", url.href);
+      return false;
+    }
+
+    return (matchers as Matcher[]).some((matcher) => {
+      if (matcher instanceof RegExp) {
+        return matcher.test(hostname);
+      } else if (typeof matcher === "string") {
+        return matcher === hostname;
       }
-      throw new Error(
-        `finicky.matchHostnames: Unrecognized hostname type "${typeof matcher}"`
-      );
+
+      return false;
     });
-  
-    return function(url: URL) {      
-      const hostname = url.hostname;
-      
-      if (!hostname) {        
-        console.warn('finicky.matchHostnames: No hostname', url.href);
-        return false;
-      }
+  };
+}
 
-      return (matchers as Matcher[]).some((matcher) => {        
-        if (matcher instanceof RegExp) {
-          return matcher.test(hostname);
-        } else if (typeof matcher === "string") {
-          return matcher === hostname;
-        }
-  
-        return false;
-      });
-    };
-  }
-
+export function matchDomains(matchers: Matcher | Matcher[]) {
+  console.warn(
+    "finicky.matchDomains is deprecated. Use finicky.matchHostnames instead."
+  );
+  return matchHostnames(matchers);
+}
